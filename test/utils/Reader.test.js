66648cbbc3510305189c1663fbacb553b6cde5bc
AlexMaderaP <alejandro.maderap@gmail.com>
2022-04-29 13:56:44 -0500

    Agregando archivo json especifico para pruebas

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -2,8 +2,9 @@ const Reader = require("../../lib/utils/Reader");
 
 describe("Unit Tests for Reader Class", () => {
     test("Read a JSON file", () => {
-        const explorers = Reader.readJsonFile("visualpartners.json");
+        const explorers = Reader.readJsonFile("./test/data/visualpartners-test.json");
 
         expect(explorers).not.toBe(undefined);
     });
 });
+
