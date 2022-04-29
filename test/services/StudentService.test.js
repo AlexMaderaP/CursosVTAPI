85b9fd32c3d6959eb6f6dec6becb22f5a68cdec3
AlexMaderaP <alejandro.maderap@gmail.com>
2022-04-29 11:30:27 -0500

    Agregando prueba de metodo para filtrar por creditos minimos

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -9,5 +9,13 @@ describe("Unit Tests for StudentService Class", () => {
         expect(studentsEmailsWithCertification.length).toBe(2);
     });
 
+    test("Getting students by minimum score", () => {
+        const students = [{email: "Todd@visualpartnership.xyz", haveCertification: true, credits: 508},
+        {email: "Sexton@visualpartnership.xyz", haveCertification: true, credits: 900}]
+        const studentsAboveCredits = StudentService.getStudentsByMinimunCredits(students,500);
+        expect(studentsAboveCredits[0].email).toBe("Todd@visualpartnership.xyz");
+        expect(studentsAboveCredits.length).toBe(2);
+    });
+
 
 });
